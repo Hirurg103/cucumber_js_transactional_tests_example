7cd01d27882d92b6360f49449a3e7422febd4d50
Dzmitry <dzmitry.kovalenok@thehamon.com>
2019-12-30 16:44:42 +0300

    Rename the app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "run_capybara_js_tests_in_transational_mode_production"
+  # config.active_job.queue_name_prefix = "cucumber_js_transactional_tests_example_production"
 
   config.action_mailer.perform_caching = false
 
